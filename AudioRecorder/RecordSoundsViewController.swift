410139bf5c5b23142334903dd271a2a14291e087
Sasha <ef1rspb@gmail.com>
2017-11-18 14:20:08 +0300

    Adding fast & slow buttons to the PlaySoundsVC

--- AudioRecorder/RecordSoundsViewController.swift
+++ AudioRecorder/RecordSoundsViewController.swift
@@ -65,5 +65,13 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
         
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "stopRecoring" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
 }
 
