ed6a106557a9194e7b126d8eda6e3bdc8a7e73f7
Sasha <ef1rspb@gmail.com>
2017-11-18 15:03:38 +0300

    Wiring up buttons (IBActions & IBOutlets)

--- AudioRecorder/PlaySoundsViewController.swift
+++ AudioRecorder/PlaySoundsViewController.swift
@@ -11,6 +11,26 @@ import UIKit
 class PlaySoundsViewController: UIViewController {
     var recordedAudioURL: URL!
     
+    // MARK: Outlets
+
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    // MARK: Actions
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
